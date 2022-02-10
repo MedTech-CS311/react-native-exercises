0db418e7515ebe2eecbfd204b7a2707c6abf1f15
Zidane Hammouda <zidane.hammouda@medtech.tn>
2022-02-10 15:03:06 +0100

    Exercice 1

--- 1. core-components/App.js
+++ 1. core-components/App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking, Image,SafeAreaView, Button} from 'react-native';
+
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+      <Image
+          style={styles.image}
+          source={require('./assets/profile.jpg')}
+        />
+        <Text style={{fontSize: 30, marginBottom: 30}}>Zidane Hammouda</Text>
+        <Text style={{marginBottom: 60}}>Life is good</Text>
+        <Button
+        onPress={()=>{ Linking.openURL('https://github.com/zidanehammouda')}}
+        title="Connect"
+        color="#841584"
+        accessibilityLabel="Navigate to github profile"
+
+        />
+      </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#302366',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: 'hidden',
+    borderWidth: 4,
+    margin: 20,
+
   },
 });
